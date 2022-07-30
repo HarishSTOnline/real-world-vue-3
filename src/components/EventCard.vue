f5d1282f2b45f3e6324503047f2ac293b733721c
Harish S <HarishSTProjects@gmail.com>
2022-07-30 21:53:32 +0530

    L3 - Complete

---
+++ src/components/EventCard.vue
@@ -0,0 +1,25 @@
+<template>
+<div class="event-card">
+  <span>@ {{ event.time }} on {{ event.date }}</span>
+  <h4>{{ event.title }}</h4>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'EventCard',
+  props: {
+    event: Object
+  }
+}
+</script>
+
+<style scoped>
+.event-card {
+  padding: 1rem;
+  width: 250px;
+  cursor: pointer;
+  border: 1px solid #ccc;
+  margin-bottom: 1rem;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,56 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div class="events">
+    <h1>Events List</h1>
+    <EventCard v-for="event in events" :key="event.id" :event="event"/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import EventCard from '@/components/EventCard.vue'
 
 export default {
-  name: "HomeView",
+  name: 'HomeView',
   components: {
-    HelloWorld,
+    EventCard,
   },
-};
+
+  data() {
+    return {
+      events: [
+        {
+          id: 222,
+          category: 'Category 1',
+          title: 'Some Title',
+          description: 'Some Description Here.',
+          location: 'California',
+          date: 'Feb 14, 2022',
+          time: '12:00PM',
+          petsAllowed: false,
+          orgainzer: 'SomeOne',
+        },
+        {
+          id: 223,
+          category: 'Category 1',
+          title: 'Some Title',
+          description: 'Some Description Here.',
+          location: 'California',
+          date: 'Feb 14, 2022',
+          time: '12:00PM',
+          petsAllowed: false,
+          orgainzer: 'SomeOne',
+        },
+      ],
+    }
+  },
+}
 </script>
+
+
+<style scoped>
+.events {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
